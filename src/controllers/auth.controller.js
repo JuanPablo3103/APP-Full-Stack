3b00518aa5ac74814f826ee7ea753ddc857b1bdc
Juan Castro <juanp.castrom@unac.edu.co>
2025-03-21 07:13:01 -0500

    💥 Add new commit
    Project is initialized

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import user from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import {createAccessToken} from "../libs/jwt.js";
+
+export const register = async (req, res) => {
+    const {email, password, username} = req.body
+
+    try {      
+        const passwordHash = await bcrypt.hash(password, 10) 
+
+        const newUser = new user ({
+            username, 
+            email,
+            password: passwordHash
+        })
+    
+        const userSaved = await newUser.save()
+        const token = await createAccessToken ({id: userSaved._id})
+
+        res.cookie("token", token) 
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt
+        })
+    }   catch (error) {
+        console.log(error);  
+    }
+};
+export const login = (req, res) => res.send('login')
